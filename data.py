2e1c4d01bb568c750e0e15420e6961c27aa53938
Kevin A <kevinalvarez27@gmail.com>
2025-06-25 05:10:42 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # File with constants
-URBAN_ROUTES_URL = 'https://cnt-5c6edf2f-3495-4402-8094-d4f1cf8fcdfa.containerhub.tripleten-services.com'
+URBAN_ROUTES_URL = 'https://cnt-88460c00-ed07-48ca-9c29-f4d5984cdb09.containerhub.tripleten-services.com/'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
