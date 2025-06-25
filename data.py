f3aae52ce532844165ddd87b9c4352519f0702f7
Kevin <kevinalvarez27@gmail.com>
2025-06-25 05:07:36 -0500

    Project 7 second change

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
 # File with constants
+URBAN_ROUTES_URL = 'https://cnt-5c6edf2f-3495-4402-8094-d4f1cf8fcdfa.containerhub.tripleten-services.com'
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER	= '1234 5678 9100'
+CARD_CODE =	'1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
