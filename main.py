9e5ff904a5945b17326b0c855133c4201eabe4af
Kevin <kevinalvarez27@gmail.com>
2025-06-25 10:28:51 -0500

    project 7 second change

--- main.py
+++ main.py
@@ -17,36 +17,36 @@ class TestUrbanRoutes:
 
     def test_select_plan(self):
         # Add in S8
-        print("function created for set route")
+        print("function created for select plan")
         pass
 
     def test_fill_phone_number(self):
         # Add in S8
-        print("function created for set route")
+        print("function created for fill in phone number")
         pass
 
     def test_fill_card(self):
         # Add in S8
-        print("function created for set route")
+        print("function created for fill card")
         pass
 
     def test_comment_for_driver(self):
         # Add in S8
-        print("function created for set route")
+        print("function created for comment for driver")
         pass
 
     def test_order_blanket_and_handkerchiefs(self):
         # Add in S8
-        print("function created for set route")
+        print("function created for order blanket and handkerchiefs")
         pass
 
     def test_order_2_ice_creams(self):
         for i in range(2):
             # Add in S8
-            print("function created for set route")
+            print("function created for order 2 ice creams")
             pass
 
     def test_car_search_model_appears(self):
         # Add in S8
-        print("function created for set route")
+        print("function created for search model appears")
         pass
